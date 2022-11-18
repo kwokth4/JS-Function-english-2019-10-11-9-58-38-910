7f8c603ea8a41f7b321c10cc65dadda8a663b450
kwokth4 <118238880+kwokth4@users.noreply.github.com>
2022-11-18 17:49:41 +0800

    Update main.js

--- 1.function1/main.js
+++ 1.function1/main.js
@@ -1,7 +1,6 @@
 function reverseString(message){
   // wirte your code here
   var msg = message.split('').reverse().join('');
-  console.log(msg);
   return msg;
 }
 reverseString('hello'); // should return 'olleh'
